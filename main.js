7bcc6742412051a9311ef95c774de9842e34aa23
angel7170 <58609234+angel7170@users.noreply.github.com>
2021-09-07 16:43:30 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,12 @@
+function setup()
+{
+    canvas = createCanvas(300, 300);
+    video = createCapture(VIDEO);
+    video.hide()
+    canvas.center()
+}
+
+function draw()
+{
+    image(video, 0, 0, 400, 400);
+}
